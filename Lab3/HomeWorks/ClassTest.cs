00ef6fece1635bfd9cb74b08bfe3f099940146a3
Evgenia Sycheva <janesych@yandex.ru>
2021-11-24 00:03:51 +0300

    Lab3

--- Lab3/HomeWorks/ClassTest.cs
+++ Lab3/HomeWorks/ClassTest.cs
@@ -5,7 +5,7 @@ namespace Lab3.HomeWorks
     /// <summary>
     /// Класс для реализации метода сравнения объектов
     /// </summary>
-    public class ClassTest : IComparable
+    public class ClassTest : IComparable 
     {
         private string name = "";
 
@@ -18,9 +18,35 @@ namespace Lab3.HomeWorks
         public string Name { get => name; set => name = value; }
         public int Age { get; set; }
 
-        public int CompareTo(object? obj)
+        public int CompareTo(object obj)
         {
-            throw new NotImplementedException();
+            ClassTest ct = (ClassTest)obj;
+            try
+            {
+                if (this.Age < ct.Age)
+                    return -1;
+                if (this.Age > ct.Age)
+                    return 1;
+                else 
+                    return 0;
+            }
+            catch (NullReferenceException)
+            {
+                throw new Exception("Нет объекта для сравнения");
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            ClassTest ct = (ClassTest)obj;
+            try
+            { 
+                return this.Name == ct.Name && this.Age == ct.Age;
+            }
+            catch (NullReferenceException)
+            {
+                return false;
+            }
         }
     }
 }
